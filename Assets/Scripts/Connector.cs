08c23a709010b3c4591f0e8ace4f0ae6fcca08d6
yishizu <ykishizu@gmail.com>
2023-01-29 21:18:00 +0900

    Update

---
+++ Assets/Scripts/Connector.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connector : MonoBehaviour
+{
+    public Vector2 size = Vector2.one * 3f;
+    // Start is called before the first frame update
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Vector2 halfSize = size*0.5f;
+        Vector3 offset = transform.position + transform.up * halfSize.y;
+        Gizmos.DrawLine(offset, offset + transform.forward);
+        Vector3 top = transform.up * size.y;
+        Vector3 side = transform.right * halfSize.x;
+        
+        Vector3 topRight = transform.position + top + side;
+        Vector3 topLeft = transform.position + top - side;
+        Vector3 botRight = transform.position + side;
+        Vector3 botLeft = transform.position - side;
+        Gizmos.DrawLine(topRight, topLeft);
+        Gizmos.DrawLine(topLeft, botLeft);
+        Gizmos.DrawLine(botLeft, botRight);
+        Gizmos.DrawLine(botRight, topRight);
+
+        Gizmos.color *= 0.5f;
+        Gizmos.DrawLine(topRight, offset);
+        Gizmos.DrawLine(topLeft, offset);
+        Gizmos.DrawLine(botLeft, offset);
+        Gizmos.DrawLine(botRight, offset);
+        
+    }
+}
